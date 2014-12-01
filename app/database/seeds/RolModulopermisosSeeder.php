21a4b3d77bd1809585a94e07886344e5581e0552
Erick Marroquin <emarroquin@cs.com.gt>
2014-12-01 15:43:56 -0600

    Seeder para permisos minimos rol CS Admin

---
+++ app/database/seeds/RolModulopermisosSeeder.php
@@ -0,0 +1,112 @@
+<?php
+
+class RolModulopermisosSeeder extends Seeder {
+	public function run() {
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 1,
+			'rolid'              => 1,
+			'modulopermisoid'    => 1,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 2,
+			'rolid'              => 1,
+			'modulopermisoid'    => 2,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 3,
+			'rolid'              => 1,
+			'modulopermisoid'    => 3,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 4,
+			'rolid'              => 1,
+			'modulopermisoid'    => 4,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 5,
+			'rolid'              => 1,
+			'modulopermisoid'    => 5,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 6,
+			'rolid'              => 1,
+			'modulopermisoid'    => 6,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 7,
+			'rolid'              => 1,
+			'modulopermisoid'    => 7,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 8,
+			'rolid'              => 1,
+			'modulopermisoid'    => 8,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 9,
+			'rolid'              => 1,
+			'modulopermisoid'    => 9,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 10,
+			'rolid'              => 1,
+			'modulopermisoid'    => 10,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 11,
+			'rolid'              => 1,
+			'modulopermisoid'    => 11,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 12,
+			'rolid'              => 1,
+			'modulopermisoid'    => 12,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 13,
+			'rolid'              => 1,
+			'modulopermisoid'    => 13,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 14,
+			'rolid'              => 1,
+			'modulopermisoid'    => 14,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+		DB::table('authrolmodulopermisos')->insert(array(
+			'rolmodulopermisoid' => 15,
+			'rolid'              => 1,
+			'modulopermisoid'    => 15,
+			'created_at'  => date_create(), 'updated_at' => date_create()
+		));
+
+	}
+}
