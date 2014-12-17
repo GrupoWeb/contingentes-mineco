3ab723499969a113968000a2b3cdf834c278962d
Erick Marroquin <emarroquin@cs.com.gt>
2014-12-17 09:00:26 -0600

    Manejo de catalogo de partidas arancelarias

--- app/routes.php
+++ app/routes.php
@@ -54,6 +54,7 @@ Route::group(array('before' => array('auth', 'cancerbero', 'menu')), function() 
 	Route::resource('requerimientos','requerimientosController');
 	Route::resource('contingentes','contingentesController');
 	Route::resource('periodos','periodosController');
+	Route::resource('partidasarancelarias','contingentepartidaController');
 	//Route::resource('catalogos/movimientos', 'movimientosController');
 
 	//=== CERTIFICADOS
