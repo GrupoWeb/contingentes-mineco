12bfe8649491e60fea6a123b03baaea266d6cbaa
Erick Marroquin <emarroquin@cs.com.gt>
2015-01-15 16:33:41 -0600

    manejo de tratados

--- app/controllers/tratadosController.php
+++ app/controllers/tratadosController.php
@@ -8,11 +8,13 @@ class tratadosController extends crudController {
 		Crud::setTablaId('tratadoid');
 		Crud::setTabla('tratados');
 
-		
+		Crud::setCampo(array('nombre'=>'Nombre Corto','campo'=>'nombrecorto'));
 	 	Crud::setCampo(array('nombre'=>'Nombre','campo'=>'nombre'));
-	 	Crud::setCampo(array('nombre'=>'Nombre Corto','campo'=>'nombrecorto'));
 	 	Crud::setCampo(array('nombre'=>'Tipo','campo'=>'tipo', 'tipo'=>'enum', 'enumarray'=>array('Importación'=>'Importación', 'Exportación'=>'Exportación'))); //NO ALMACENA EL VALOR DEL ENUM
+	 	Crud::setCampo(array('nombre'=>'Contingentes', 'campo'=>'(SELECT count(*) FROM contingentes AS c WHERE c.tratadoid = tratados.tratadoid)', 'class'=>'text-right', 'editable'=>false));
 	 	Crud::setCampo(array('nombre'=>'Activo','campo'=>'activo', 'tipo'=>'bool'));
+
+	 	Crud::setBotonExtra(array('url'=>'contingentes?tratado=','icon'=>'glyphicon glyphicon-certificate','titulo'=>'Asignar Contingentes'));
 	 
 	 	Crud::setPermisos(Cancerbero::tienePermisosCrud('tratados'));
 	}
