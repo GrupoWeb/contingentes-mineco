a9275d7b4099dce61a5dfb5d0b252f567c7e4eda
Erick Marroquin <emarroquin@cs.com.gt>
2015-01-20 09:39:44 -0600

    fix nombre contingente

--- app/controllers/cuentacorrienteController.php
+++ app/controllers/cuentacorrienteController.php
@@ -3,7 +3,7 @@
 class cuentacorrienteController extends BaseController {
 	
 	public function index() {
-		$contingente = contingente::find(1);
+		$contingente = Contingente::find(1);
 		$producto    = Producto::find($contingente->productoid);
 		$tratado     = Tratado::find($contingente->tratadoid);
 
