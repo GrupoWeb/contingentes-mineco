1b73de4aba4278e4080e7bd4a3a989c231bbe2d3
Erick Marroquin <emarroquin@cs.com.gt>
2015-01-26 10:26:08 -0600

    Asignaciones

--- app/models/Usuariocontingente.php
+++ app/models/Usuariocontingente.php
@@ -4,13 +4,17 @@ class Usuariocontingente extends Eloquent {
 
 	protected $primaryKey = 'usuariocontingenteid';
 
-	public static function getContingentes() {
-		return DB::table('usuariocontingentes AS uc')
+	public static function getContingentes($asignacion=false) {
+		$query = DB::table('usuariocontingentes AS uc')
 			->select('uc.contingenteid', 't.nombrecorto AS tratado', 'p.nombre AS producto', 'c.tratadoid')
 			->leftJoin('contingentes AS c', 'uc.contingenteid', '=', 'c.contingenteid')
 			->leftJoin('tratados AS t', 'c.tratadoid', '=', 't.tratadoid')
 			->leftJoin('productos AS p', 'c.productoid', '=', 'p.productoid')
-			->where('uc.usuarioid', Auth::id())
-			->get();
+			->where('uc.usuarioid', Auth::id());
+
+		if($asignacion)
+			$query->where('asignacion', 1);
+
+		return $query->get();
 	}
 }
