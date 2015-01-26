7564228aedf688491bb8f32481f2854901977786
Erick Marroquin <emarroquin@cs.com.gt>
2015-01-26 15:28:56 -0600

    modelo usuario contingentes

--- app/models/Usuariocontingente.php
+++ app/models/Usuariocontingente.php
@@ -5,16 +5,12 @@ class Usuariocontingente extends Eloquent {
 	protected $primaryKey = 'usuariocontingenteid';
 
 	public static function getContingentes($asignacion=false) {
-		$query = DB::table('usuariocontingentes AS uc')
+		return DB::table('usuariocontingentes AS uc')
 			->select('uc.contingenteid', 't.nombrecorto AS tratado', 'p.nombre AS producto', 'c.tratadoid')
 			->leftJoin('contingentes AS c', 'uc.contingenteid', '=', 'c.contingenteid')
 			->leftJoin('tratados AS t', 'c.tratadoid', '=', 't.tratadoid')
 			->leftJoin('productos AS p', 'c.productoid', '=', 'p.productoid')
-			->where('uc.usuarioid', Auth::id());
-
-		if($asignacion)
-			$query->where('asignacion', 1);
-
-		return $query->get();
+			->where('uc.usuarioid', Auth::id())
+			->get();
 	}
 }
