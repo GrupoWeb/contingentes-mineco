4ed63ba507e4a761c638860d80f4c1294c98486b
Erick Marroquin <emarroquin@cs.com.gt>
2015-02-27 09:12:32 -0600

    dashboard

--- app/routes.php
+++ app/routes.php
@@ -89,7 +89,7 @@ Route::group(array('before' => array('tratados')), function() {
 
 		//=== REPORTES
 		Route::resource('cuentacorriente', 'cuentacorrienteController', array('only'=>array('index','store')));
-		Route::resource('empresas', 'empresasController', array('only'=>array('index')));
+		Route::resource('empresas', 'empresasController', array('only'=>array('index', 'store')));
 	  
 	  //=== ADMIN
 	  Route::resource('usuarioswebservice', 'usuarioswebserviceController');
