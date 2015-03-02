cc76c704858a41c935d3da93c6dd502b5879a503
Jose Galindo (Mac) <jgalindo@cs.com.gt>
2015-03-02 13:50:37 -0600

    Migration para getsaldo correcto. (toma en cuenta el usuario)

---
+++ app/database/migrations/2015_03_02_134738_migration_functiongetsaldousuario.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MigrationFunctiongetsaldousuario extends Migration {
+
+	public function up() {
+		DB::unprepared('DROP FUNCTION IF EXISTS getSaldo');
+		DB::unprepared("CREATE FUNCTION getSaldo(aContingenteId INTEGER, aUsuarioId INTEGER) RETURNS DECIMAL(20,5)
+										BEGIN
+										  RETURN (SELECT 
+										    IFNULL(SUM(cantidad), 0) AS saldo 
+										  FROM movimientos AS m
+										  LEFT JOIN periodos AS p USING(periodoid)
+										  LEFT JOIN contingentes AS c USING(contingenteid)
+										  LEFT JOIN tratados AS t USING(tratadoid)
+										  LEFT JOIN tipotratados AS tt USING(tipotratadoid)
+										  WHERE p.contingenteid = aContingenteId
+										  AND IF(tt.asignacion = 1, (m.tipo IN('Asignacion','Certificado') AND m.usuarioid=aUsuarioId), (m.tipo IN('Cuota','Certificado'))));
+										END");
+	}
+
+	public function down() {
+		DB::unprepared('DROP FUNCTION IF EXISTS getSaldo');
+		DB::unprepared("CREATE FUNCTION getSaldo(aContingenteId INTEGER, aUsuarioId INTEGER) RETURNS DECIMAL(20,5)
+										BEGIN
+										  RETURN (SELECT 
+										    IFNULL(SUM(cantidad), 0) AS saldo 
+										  FROM movimientos AS m
+										  LEFT JOIN periodos AS p USING(periodoid)
+										  LEFT JOIN contingentes AS c USING(contingenteid)
+										  LEFT JOIN tratados AS t USING(tratadoid)
+										  LEFT JOIN tipotratados AS tt USING(tipotratadoid)
+										  WHERE p.contingenteid = aContingenteId
+										  AND IF(tt.asignacion = 1, (m.tipo IN('Asignacion','Certificado')), (m.tipo IN('Cuota','Certificado'))));
+										END");
+	}
+
+}
