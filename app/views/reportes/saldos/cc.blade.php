562c3ce12dbcd74c8c48905c8f98df96ad6f610d
Erick Marroquin <emarroquin@cs.com.gt>
2015-03-03 20:05:54 -0600

    Correo de inscripcion

---
+++ app/views/reportes/saldos/cc.blade.php
@@ -0,0 +1,65 @@
+@extends('template/reporte')
+
+@section('content')
+	{{ HTML::script('js/highcharts-exporting.js') }}
+	{{ HTML::script('js/highcharts.js') }}
+
+	@include('partials/reportes/header')
+
+	<div id="container"></div>
+
+	<script>
+		$(function () {
+	    $('#container').highcharts({
+	        chart: {
+	            type: 'column'
+	        },
+	        title: {
+	            text: ''
+	        },
+	        xAxis: {
+	            categories: [
+	            	@foreach($empresas as $empresa)
+	            		'{{ $empresa["nombre"] }}', 
+	            	@endforeach
+	            ]
+	        },
+	        yAxis: {
+	            min: 0,
+	            title: {
+	                text: ''
+	            }
+	        },
+	        legend: {
+	            reversed: true
+	        },
+	        credits : {
+	        	enabled: false
+	        },
+	        exporting: {
+	        	enabled: true
+	        },
+	        plotOptions: {
+	            series: {
+	                stacking: 'normal'
+	            }
+	        },
+	        series: [{
+	            name: 'Asignado',
+	            data: [
+	            	@foreach($empresas as $empresa)
+	            		{{ $empresa['asignado']. ',' }}
+	            	@endforeach
+	            ]
+	        }, {
+	            name: 'Consumido',
+	            data: [
+	            	@foreach($empresas as $empresa)
+	            		{{ $empresa['consumido'] . ',' }}
+	            	@endforeach
+	            ]
+	        }]
+	    });
+		});
+	</script>
+@stop
