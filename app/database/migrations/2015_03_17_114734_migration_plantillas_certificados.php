1200991719abef127c8c30bba88c8a03e369b6f5
Erick Marroquin <emarroquin@cs.com.gt>
2015-03-17 12:16:56 -0600

    fix migration

--- app/database/migrations/2015_03_17_114734_migration_plantillas_certificados.php
+++ app/database/migrations/2015_03_17_114734_migration_plantillas_certificados.php
@@ -1,26 +1,25 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class MigrationPlantillasCertificados extends Migration {
 
 	public function up() {
-		Schema::create('plantillascertificados', function(Blueprint $table){
+		Schema::create('plantillascertificados', function($table){
 			$table->increments('plantillaid');
 			$table->string('nombre');
 			$table->string('vista', 50);
 			$table->timestamps();
 		});	
 
-		Schema::table('contingentes', function(Blueprint $table) {
+		Schema::table('contingentes', function($table) {
 			$table->integer('plantillaid')->unsigned()->nullable()->after('textocertificado');
 			$table->foreign('plantillaid')->references('plantillaid')->on('plantillascertificados')->onUpdate('CASCADE')->onDelete('RESTRICT');
 		});
 	}
 
 	public function down() {
-		Schema::table('contingentes', function(Blueprint $table) {
+		Schema::table('contingentes', function($table) {
 			$table->dropForeign('contingentes_plantillaid_foreign');
 			$table->dropColumn('plantillaid');
 		});
