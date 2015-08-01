33e188563833c4ed2f4a632db93dd3ec7804759c
CSMacbook <jgalindo@cs.com.gt>
2015-08-01 07:28:25 -0600

    Se agrego el campo "responsable" a la tabla de contingentes para que en los correos se envie el nombre del responsable.

--- app/config/contingentes.php
+++ app/config/contingentes.php
@@ -14,6 +14,7 @@ return array(
 
 	'roladmin'        => array(1,2,4),
 	'rolempresa'			=> array(3),
+	'roldace'					=> array(2),
 	'tratadosExclude' => array('cuentacorriente', 'login', 'reset','cancerbero','paises',
 		'productos','requerimientos','solicitud','', 'unidadesmedida','inicio', 
 		'usuarios', 'usuarioempresas', 'roles','cuentacorrienteempresas','usuariosextra',
