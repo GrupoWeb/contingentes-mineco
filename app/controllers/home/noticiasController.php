3de514ee963646b75e1b412f45bfdf529363e5fb
Valeriano Chete <vchete@cs.com.gt>
2015-11-25 17:41:43 -0600

    documentacion codigo fuente carpeta home

--- app/controllers/home/noticiasController.php
+++ app/controllers/home/noticiasController.php
@@ -4,7 +4,7 @@ class noticiasController extends BaseController {
 	
 	public function index() {
 		$noticias = Noticia::getNoticias();
-
+		//retorna a la vista
 		return View::make('home.noticias', ['noticias'=>$noticias]);
 	}
 }
