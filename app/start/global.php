f2a9bea7867c786e3a183a97fd29ef5ce43087e1
CSMacbook <jgalindo@cs.com.gt>
2015-11-25 20:25:34 -0600

    Site down

--- app/start/global.php
+++ app/start/global.php
@@ -19,7 +19,6 @@ App::error(function(Exception $exception, $code) {
 });
 
 App::down(function() {
-  return Response::make("Be right back!", 503);
+	return Response::view('template/mantenimiento', array(), 503);
 });
-
 require app_path().'/filters.php';
