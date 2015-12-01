1f7ebee8e05b1bad2f09800f8fd145862d7a2a31
Valeriano Chete <vchete@cs.com.gt>
2015-12-01 11:10:56 -0600

    logos nuevos para toda la pagina

--- app/views/template/home.blade.php
+++ app/views/template/home.blade.php
@@ -34,7 +34,7 @@
           <span class="icon-bar"></span>
         </button>
         <a class="navbar-brand" href="/">
-          <img src="/images/logo-menu.png" alt="Ministerio de economía - DACE">
+          <img src="/images/logo-menu.jpg" alt="Ministerio de economía - DACE">
         </a>
       </div>
       <!-- Collect the nav links, forms, and other content for toggling -->
