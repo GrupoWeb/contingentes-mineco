70f1d71c338f15103af220a19a9973b5627b165e
Erick Marroquin <emarroquin@cs.com.gt>
2015-12-04 17:45:09 -0600

    fix header reporte de utilizacion

--- app/views/reportes/utilizaciones.blade.php
+++ app/views/reportes/utilizaciones.blade.php
@@ -65,8 +65,6 @@
 						if ($cuantos==0) $cuantos=1;
 					?>
 					<tr>
-						<td rowspan="{{ $cuantos }}" style="vertical-align: middle;">{{ $nit }}</td>
-						<td rowspan="{{ $cuantos }}" style="vertical-align: middle;">{{ $nombre }}</td>
 						@if($esasignacion==1)
 							<td rowspan="{{ $cuantos }}" class="text-right">{{ number_format($movimientos['asignado'], 3) }}</td>
 							<td rowspan="{{ $cuantos }}" class="text-right">{{ number_format($movimientos['adjudicado'], 3) }}</td>
@@ -74,6 +72,8 @@
 						@else
 							<td rowspan="{{ $cuantos }}" colspan="3" class="text-right" style="vertical-align: middle;">{{ number_format($movimientos['adjudicado'], 3) }}</td>
 						@endif
+						<td rowspan="{{ $cuantos }}" style="vertical-align: middle;">{{ $nit }}</td>
+						<td rowspan="{{ $cuantos }}" style="vertical-align: middle;">{{ $nombre }}</td>
 						<?php $i=1; ?>
 						@if(count($movimientos['movimientos'])==0)
 							<td colspan="12">&nbsp;</td>
