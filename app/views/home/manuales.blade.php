c7dd605fbec9a5d6eded626ce5e6174440fc0384
Josue Perez <jperez@cs.com.gt>
2015-12-15 12:30:47 -0600

    Manual de usuario con el rol de exportador se agrego el link de descarga en la pagina de manuales

--- app/views/home/manuales.blade.php
+++ app/views/home/manuales.blade.php
@@ -3,5 +3,6 @@
 @section('content')
 	<div class="contenido">
     <h1 class="titulo">Manuales</h1>
+    <a href="/manuales/Manual Contingentes (Rol Exportador).pdf">Manual Contingente Rol de Exportador</a>
 	</div>
 @stop
