8fcec82f1562f1c8e02e415206f2884e87770f58
Jose Galindo <jgalindo@softlogic.com.gt>
2020-03-10 14:18:08 -0600

    docker y gitignore

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -1,13 +1,14 @@
 <?php
-return array(
-	'fetch' => PDO::FETCH_CLASS,
-	'default' => 'mysql',
-	'connections' => array(
-		'mysql' => array(
-			'host'      => 'localhost',
-			'database'  => 'contingentes',
-			'username'  => 'root',
-			'password'  => 'root',
-		),
-	),
-);
+
+return [
+    'fetch'       => PDO::FETCH_CLASS,
+    'default'     => 'mysql',
+    'connections' => [
+        'mysql' => [
+            'host'     => 'host.docker.internal',
+            'database' => 'contingentes',
+            'username' => 'root',
+            'password' => 'secret',
+        ],
+    ],
+];
